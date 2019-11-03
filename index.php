bc41d79b1114d0b51a6a460ae7b8121473dcb8c2
ThomasBoom89 <t.boockmeyer@googlemail.com>
2019-11-03 17:19:50 +0100

    index angepasst

--- index.php
+++ index.php
@@ -1,15 +1,8 @@
 <?php include "template.php" ?>
 <body class="bg-dark text-white text-center">
 	<div>
-		<p>Eine Seite zur Berechnung mathematischer und elektrischer Größen!</p>
-		<br>
-		<img src="" class="" style="img-fluid" alt="">
-	</div>
-  <br>
-	<h1>15163513651</h1>
-	<br>
-	<h2>hcvbsdahfcvblhsd</h2>
-	<span class="meta">Erstellt von Andrej Scherer, am 03.11.2019</span>
-	<br>
-	<br>
+		<h1>Header 1</h1>
+        <br>
+	    <h2>Header 2</h2>
+    </div>
 </body>
