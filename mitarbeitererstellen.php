845bb329229a26a15f3fc32c613b020099e9120d
AndScher88 <andy.scherer1887@gmail.com>
2019-11-04 09:48:16 +0100

    Nächste Card bearbeitet (Mitarbeiter erstellen)

---
+++ mitarbeitererstellen.php
@@ -0,0 +1,9 @@
+<?php include "template.php" ?>
+<title>Mitarbeiter erstellen</title>
+<body class="text-center">
+  <div>
+    <h1>Mitarbeiter erstellen</h1>
+    <br>
+    <h3>Bitte hier die Daten des neuen Mitarbeiters eingeben: </h3>
+  </div>
+</body>
