64962e69957d0e442d42f137e70b0510e5687b7c
AndScher88 <andy.scherer1887@gmail.com>
2019-11-12 19:31:29 +0100

    $_GET oder $_POST

--- mitarbeitererstellen.php
+++ mitarbeitererstellen.php
@@ -9,25 +9,25 @@
             </div>
             <div class = "form-row">
                 <div class = "col">
-                    <input type="text" class="form-control" id="vorname" aria-describedby="vornamelabel" placeholder="Max">
+                    <input type="text" name="vorname" class="form-control" id="vorname1" aria-describedby="vornamelabel" placeholder="Max">
                     <label id="vornamelabel" class="form-text text-muted">Vorname</label>
                 </div>
                 <div class="col">
-                    <input type="text" class="form-control" id="nachname" aria-describedby="nachnamelabel" placeholder="Mustermann">
+                    <input type="text" name="nachname" class="form-control" id="nachname1" aria-describedby="nachnamelabel" placeholder="Mustermann">
                     <label id="nachnamelabel" class="form-text text-muted">Nachname</label>
                 </div>
                 <div class="col">
-                    <input type="date" class="form-control" id="geburtsdatum" aria-describedby="geburtsdatumlabel"placeholder="dd.MM.YYYY">
+                    <input type="date" name="gebdatum" class="form-control" id="geburtsdatum1" aria-describedby="geburtsdatumlabel"placeholder="dd.MM.YYYY">
                     <label id="geburtsdatumlabel" class="form-text text-muted">Geburtsdatum</label>
                 </div>
             </div>
             <div class="form-row">
                 <div class="col">
-                    <input type="text" class="form-control" id="strasse" aria-describedby="wohnortlabel" placeholder="Lindenstraße">
+                    <input type="text" name="strasse" class="form-control" id="strasse1" aria-describedby="wohnortlabel" placeholder="Lindenstraße">
                     <label id="wohnortlabel" class="form-text text-muted">Wohnort</label>
                 </div>
                 <div class="col">
-                    <input type="text" class="form-control" id="hausnummer" aria-describedby="hausnummerlabel" placeholder="10a">
+                    <input type="text" name="hausnummer" class="form-control" id="hausnummer1" aria-describedby="hausnummerlabel" placeholder="10a">
                     <label id="hausnummerlabel" class="form-text text-muted">Hausnummer</label>
                 </div>
                 <div class="col">
@@ -35,13 +35,13 @@
                     <label id="stadtlabel" class="form-text text-muted">Stadt</label>
                 </div>
                 <div class="col">
-                    <input type="text" class="form-control" id="postleitzahl" aria-describedby="postleitzahllabel" placeholder="22880">
+                    <input type="text" class="form-control" id="postleitzahl1" aria-describedby="postleitzahllabel" placeholder="22880">
                     <label id="postleitzahllabel" class="form-text text-muted">Plz</label>
                 </div>
             </div>
             <div class="form-row">
                 <div class="col">
-                    <input type="tel" class="form-control" id="telefonnummer" aria-describedby="telefonnummerlabel" placeholder="015112345678">
+                    <input type="tel" name="telefonnummer" class="form-control" id="telefonnummer1" aria-describedby="telefonnummerlabel" placeholder="015112345678">
                     <label id="telefonnummerlabel" class="form-text text-muted">Telefonnummer</label>
                 </div>
                 <div class="col">
@@ -62,8 +62,32 @@
                 </div>
             </div>
             <div class="btn-group" role="group" aria-label="hinzufügen">
-                <a href="mitarbeiterneu.php" class="btn btn-secondary">Mitarbeiter hinzufügen</a>
+                <button class="btn btn-secondary" value="Submit">Mitarbeiter hinzufügen</button>
             </div>
         </form>
     </div>
+    <?php
+    $vorname = "";
+    $nachname = "";
+    $gebdatum = "";
+    $strasse = "";
+    $hausnummer = "";
+    $stadt = "";
+    $plz = "";
+    $telnummer = "";
+    $email = "";
+    $abteilung = "";
+
+    if ($_GET["vorname"] & $_GET["nachname"] & $_GET["gebdatum"] & $_GET["strasse"] & $_GET["hausnummer"] & $_GET["telefonnummer"])
+    {
+        $vorname = $_GET["vorname"];
+        $nachname = $_GET["nachname"];
+        $gebdatum = $_GET["gebdatum"];
+        $strasse = $_GET["strasse"];
+        $hausnummer = $_GET["hausnummer"];
+        $telnummer = $_GET["telefonnummer"];
+        echo "<p>Der vollständige Name lauet: $vorname $nachname geboren am: $gebdatum und wohnt in der Straße: $strasse $hausnummer. Errichbar ist Herr/Frau $nachname unter der Nummer: $telnummer </p>";
+    }
+
+    ?>
 </body>
