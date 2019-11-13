5f3ed3fd03a1bd7fe0f13966cb03de758da39b3a
Andrej Scherer <Scherer@set.local>
2019-11-13 09:13:28 +0100

    HTML5 Funktion required hinzugefügt. aktion und method in die Form integriert und das php-skript entfernt.

--- mitarbeitererstellen.php
+++ mitarbeitererstellen.php
@@ -3,55 +3,55 @@
 <body class="text-center">
     <div class = "container-fluid">
         <h3 class="text-muted">Mitarbeiter erstellen</h3>
-        <form class = "text-left">
+        <form class = "text-left" action="mitarbeiterneu.php" method="post">
             <div class="text-left">
                 <label class="text-muted">Bitte hier die Daten des neuen Mitarbeiters eingeben: </label>
             </div>
             <div class = "form-row">
                 <div class = "col">
-                    <input type="text" name="vorname" class="form-control" id="vorname1" aria-describedby="vornamelabel" placeholder="Max">
+                    <input type="text" name="vorname" class="form-control" id="vorname1" aria-describedby="vornamelabel" placeholder="Max" required>
                     <label id="vornamelabel" class="form-text text-muted">Vorname</label>
                 </div>
                 <div class="col">
-                    <input type="text" name="nachname" class="form-control" id="nachname1" aria-describedby="nachnamelabel" placeholder="Mustermann">
+                    <input type="text" name="nachname" class="form-control" id="nachname1" aria-describedby="nachnamelabel" placeholder="Mustermann" required>
                     <label id="nachnamelabel" class="form-text text-muted">Nachname</label>
                 </div>
                 <div class="col">
-                    <input type="date" name="gebdatum" class="form-control" id="geburtsdatum1" aria-describedby="geburtsdatumlabel"placeholder="dd.MM.YYYY">
+                    <input type="date" name="gebdatum" class="form-control" id="geburtsdatum1" aria-describedby="geburtsdatumlabel"placeholder="dd.MM.YYYY" required>
                     <label id="geburtsdatumlabel" class="form-text text-muted">Geburtsdatum</label>
                 </div>
             </div>
             <div class="form-row">
                 <div class="col">
-                    <input type="text" name="strasse" class="form-control" id="strasse1" aria-describedby="wohnortlabel" placeholder="Lindenstraße">
+                    <input type="text" name="strasse" class="form-control" id="strasse1" aria-describedby="wohnortlabel" placeholder="Lindenstraße" required>
                     <label id="wohnortlabel" class="form-text text-muted">Wohnort</label>
                 </div>
                 <div class="col">
-                    <input type="text" name="hausnummer" class="form-control" id="hausnummer1" aria-describedby="hausnummerlabel" placeholder="10a">
+                    <input type="text" name="hausnummer" class="form-control" id="hausnummer1" aria-describedby="hausnummerlabel" placeholder="10a" required>
                     <label id="hausnummerlabel" class="form-text text-muted">Hausnummer</label>
                 </div>
                 <div class="col">
-                    <input type="text" class="form-control" id="stadt" aria-describedby="stadtlabel" placeholder="Hamburg">
+                    <input type="text" class="form-control" id="stadt" aria-describedby="stadtlabel" placeholder="Hamburg" required>
                     <label id="stadtlabel" class="form-text text-muted">Stadt</label>
                 </div>
                 <div class="col">
-                    <input type="text" class="form-control" id="postleitzahl1" aria-describedby="postleitzahllabel" placeholder="22880">
+                    <input type="text" class="form-control" id="postleitzahl1" aria-describedby="postleitzahllabel" placeholder="22880" required>
                     <label id="postleitzahllabel" class="form-text text-muted">Plz</label>
                 </div>
             </div>
             <div class="form-row">
                 <div class="col">
-                    <input type="tel" name="telefonnummer" class="form-control" id="telefonnummer1" aria-describedby="telefonnummerlabel" placeholder="015112345678">
+                    <input type="tel" name="telefonnummer" class="form-control" id="telefonnummer1" aria-describedby="telefonnummerlabel" placeholder="015112345678" required>
                     <label id="telefonnummerlabel" class="form-text text-muted">Telefonnummer</label>
                 </div>
                 <div class="col">
-                    <input type="email" class="form-control" id="email" aria-describedby="emaillabel" placeholder="max.mustermann@gmail.com">
+                    <input type="email" class="form-control" id="email" aria-describedby="emaillabel" placeholder="max.mustermann@gmail.com" required>
                     <label id="emaillabel" class="form-text text-muted">E-Mail Adresse</label>
                 </div>
             </div>
             <div class="form-row">
                 <div class="col">
-                    <select class="custom-select" id="abteilung">
+                    <select class="custom-select" id="abteilung" required>
                         <option selected>Wählen...</option>
                         <option value="1">Human Resources</option>
                         <option value="2">Engineering</option>
@@ -66,28 +66,4 @@
             </div>
         </form>
     </div>
-    <?php
-    $vorname = "";
-    $nachname = "";
-    $gebdatum = "";
-    $strasse = "";
-    $hausnummer = "";
-    $stadt = "";
-    $plz = "";
-    $telnummer = "";
-    $email = "";
-    $abteilung = "";
-
-    if ($_GET["vorname"] & $_GET["nachname"] & $_GET["gebdatum"] & $_GET["strasse"] & $_GET["hausnummer"] & $_GET["telefonnummer"])
-    {
-        $vorname = $_GET["vorname"];
-        $nachname = $_GET["nachname"];
-        $gebdatum = $_GET["gebdatum"];
-        $strasse = $_GET["strasse"];
-        $hausnummer = $_GET["hausnummer"];
-        $telnummer = $_GET["telefonnummer"];
-        echo "<p>Der vollständige Name lauet: $vorname $nachname geboren am: $gebdatum und wohnt in der Straße: $strasse $hausnummer. Errichbar ist Herr/Frau $nachname unter der Nummer: $telnummer </p>";
-    }
-
-    ?>
 </body>
