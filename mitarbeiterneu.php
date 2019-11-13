a3f85d586161a292a5fdcaf75eb41d26e4e8f37a
Andrej Scherer <Scherer@set.local>
2019-11-13 09:14:57 +0100

    Das php-skript aus mitarbeitererstellen eingefügt

--- mitarbeiterneu.php
+++ mitarbeiterneu.php
@@ -3,5 +3,36 @@ namespace classes\database;
 include "classes\database\DatabaseConnector.php";
 
 $var = DatabaseConnector::getAccess();
-print_r($var);
+//print_r($var);
 ?>
+<body>
+<?php
+include "template.php";
+$vorname;
+$nachname;
+$gebdatum;
+$strasse;
+$hausnummer;
+$stadt;
+$plz;
+$telnummer;
+$email;
+$abteilung;
+
+$vorname = $_POST["vorname"];
+$nachname = $_POST["nachname"];
+$gebdatum = $_POST["gebdatum"];
+$strasse = $_POST["strasse"];
+$hausnummer = $_POST["hausnummer"];
+$telnummer = $_POST["telefonnummer"];
+
+if ($vorname != "" && $nachname != "")
+{
+    echo "<p>Die aufgenommenen Daten wurden erfolgreich gespreichert!</p>";
+}
+else
+{
+  echo "Es ist ein Fehler aufgefreten!";
+}
+?>
+</body>
