05475f77b9f1171234ee5158de04ba62eff73785
AndScher88 <andy.scherer1887@gmail.com>
2020-01-25 16:41:39 +0100

    das Ändern von Mitarbeiterdaten möglich sein.

--- mitarbeiterupdate.php
+++ mitarbeiterupdate.php
@@ -1,1 +1,42 @@
 <?php
+require_once __DIR__ . '/autoloader.php';
+
+use classes\employee\Employee;
+
+Session_Start();
+if (!isset($_SESSION['login']) || $_SESSION['login'] === 0) {
+    Header('Location: login.php');
+    exit();
+}
+
+$empl_id = $_POST['id'];
+$vorname = $_POST['vorname'];
+$nachname = $_POST['nachname'];
+$gebdatum = $_POST['gebdatum'];
+$strasse = $_POST['strasse'];
+$hausnummer = $_POST['hausnummer'];
+$stadt = $_POST['stadt'];
+$postleitzahl = $_POST['postleitzahl'];
+$telefonnummer = $_POST['telefonnummer'];
+$email = $_POST['email'];
+
+if (isset($_POST['id'])) {
+    if ($_POST['id'] > 0) {
+        $empl_id = $_POST['id'];
+        $employee = new Employee();
+        $employee->setEmplId($empl_id);
+        $employee->setVorname($vorname);
+        $employee->setNachname($nachname);
+        $employee->setGebdatum($gebdatum);
+        $employee->setStrasse($strasse);
+        $employee->setHausnummer($hausnummer);
+        $employee->setStadt($stadt);
+        $employee->setPostleitzahl($postleitzahl);
+        $employee->setTelefonnummer($telefonnummer);
+        $employee->setEmail($email);
+        $employee->queryUpdateEmployee();
+
+        header('Location: mitarbeiteranzeigen.php');
+    }
+
+}
