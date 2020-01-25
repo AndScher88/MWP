14377c49f81f07bea0fdd1bbb895a96c3d82828c
AndScher88 <andy.scherer1887@gmail.com>
2020-01-25 16:55:41 +0100

    Inputfelder Stadt und Postleitzahl getauscht

--- mitarbeiteranzeigen.php
+++ mitarbeiteranzeigen.php
@@ -1,57 +1,22 @@
 <?php
 require_once __DIR__ . '/autoloader.php';
-require_once "template.php";
-use classes\database\DatabaseConnector;
+require_once 'template.php';
 
+use classes\frontend\Table;
+use classes\employee\Employee;
 
-$conn = DatabaseConnector::getAccess();
+$employee = new Employee();
+$employee->queryEmployees();
+$resultEmployee = $employee->getResult();
 
-$tabelle = new \classes\mitarbeiter\baueTabelle();
-
-$sql ='SELECT * FROM mitarbeiterdaten ORDER BY vorname';
-
-$result = $conn->query($sql);
-$conn->close();
-
-
+$table = new Table();
+$table->setResult($resultEmployee);
 ?>
 
 <body >
 <h3 class="text-center">Daten aller Mitarbeiter</h3>
-<table class="table table-secondary">
-    <thead>
-        <tr class="text-left">
-            <th>Vorname </th>
-            <th>Nachname </th>
-            <th>Anschrift</th>
-            <th>Stadt</th>
-            <th>Postleitzahl</th>
-            <th>Geburtsdatum</th>
-            <th>Telefonnummer</th>
-            <th>E-Mail-Adresse</th>
-            <th></th>
-            <th></th>
-        </tr>
-    </thead>
-
-    <tbody>
-        <?php  foreach ($result as $mitarbeiter): ?>
-            <tr class="text-left">
-                <td><?php echo htmlspecialchars($mitarbeiter['vorname']); ?></td>
-                <td><?php echo htmlspecialchars($mitarbeiter['nachname']); ?></td>
-                <td><?php echo htmlspecialchars($mitarbeiter['strasse']) . " "
-                             . htmlspecialchars($mitarbeiter['hausnummer']); ?></td>
-                <td><?php echo htmlspecialchars($mitarbeiter['stadt']); ?></td>
-                <td><?php echo htmlspecialchars($mitarbeiter['postleitzahl']); ?></td>
-                <td><?php echo htmlspecialchars($mitarbeiter['geburtsdatum']); ?></td>
-                <td><?php echo htmlspecialchars($mitarbeiter['telefonnummer']); ?></td>
-                <td><?php echo htmlspecialchars($mitarbeiter['email']); ?></td>
-                <td><a href="mitarbeiterbearbeiten.php?id=<?php htmlspecialchars($mitarbeiter['id'])?>">Bearbeiten...</a></td>
-                <td><a href="mitarbeiterloeschen.php?id=<?php htmlspecialchars($mitarbeiter['id'])?>">Löschen...</a></td>
-            </tr>
-        <?php endforeach; ?>
-    </tbody>
-
+<?php $table->createTableHead();  ?>
+<?php $table->createTableBody();  ?>
 </table>
 
 
