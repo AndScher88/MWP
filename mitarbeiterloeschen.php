71393189a07088ff82b07a9745800eec0dc23f12
AndScher88 <andy.scherer1887@gmail.com>
2020-01-25 16:50:57 +0100

    das löschen von Mitarbeitern möglich sein.

--- mitarbeiterloeschen.php
+++ mitarbeiterloeschen.php
@@ -1,1 +1,19 @@
 <?php
+require_once __DIR__ . '/autoloader.php';
+
+use classes\employee\Employee;
+
+Session_Start();
+if (!isset($_SESSION['login']) || $_SESSION['login'] === 0) {
+    Header('Location: login.php');
+    exit();
+}
+
+
+$empl_id = $_GET['id'];
+
+$employee = new Employee();
+$employee->setEmplId($empl_id);
+$employee->queryDeleteEmployee();
+
+header('Location: mitarbeiteranzeigen.php');
