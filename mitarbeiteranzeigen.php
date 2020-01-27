9c45576d89b3afbe70a39953b48c9cb9d1c3cc5b
AndScher88 <andy.scherer1887@gmail.com>
2020-01-27 10:22:34 +0100

    wurden die Klassenaufrufe ausgelagert

--- mitarbeiteranzeigen.php
+++ mitarbeiteranzeigen.php
@@ -1,29 +1,14 @@
 <?php
 require_once __DIR__ . '/autoloader.php';
 require_once 'template.php';
-
-use classes\frontend\Table;
 use classes\employee\Employee;
-
-$employeeHead = new Employee();
-$employeeHead->queryEmployees();
-$resultEmployeeHead = $employeeHead->getResult();
-$employeeBody = new Employee();
-$employeeBody->queryEmployees();
-$resultEmployeeBody = $employeeBody->getResult();
 ?>
 
 <body >
 <h3 class="text-center">Daten aller Mitarbeiter</h3>
 <?php
-$tableHead = new Table();
-$tableHead->setResult($resultEmployeeHead);
-$tableHead->createTableHead();
-$tableBody = new Table();
-$tableBody->setResult($resultEmployeeBody);
-$tableBody->createTableBody();
+$employee = new Employee();
+$employee->createEmployeeTable();
 ?>
 </table>
-
-
 </body>
